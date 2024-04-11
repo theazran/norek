339f3a9cdcac8dcaa6c1d5d45d8c42fbe87a5c5a
M ASRAN <62879334+theazran@users.noreply.github.com>
2024-04-11 07:36:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+
+const banksData = fs.readFileSync(path.join(__dirname, "./db/banks.json"));
+const profileData = fs.readFileSync(path.join(__dirname, "./db/profile.json"));
+const profile = JSON.parse(profileData);
+const banks = JSON.parse(banksData);
+
+app.get("/", (req, res) => {
+    res.render("index", { banks, profile });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
